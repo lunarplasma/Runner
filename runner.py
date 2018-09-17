82fd129143234ae4359cb5e27ee26923f4367417
ITCS\Johng <lunarplasma@gmail.com>
2018-09-17 10:06:39 +0100

    Tidied up the checking of the process.

--- runner.py
+++ runner.py
@@ -4,7 +4,6 @@ from os.path import isfile
 
 """
 Trick learned from: https://www.alexkras.com/how-to-restart-python-script-after-exception-and-run-it-forever/
-
 """
 
 if __name__ == '__main__':
@@ -33,10 +32,7 @@ if __name__ == '__main__':
                 print(" == Starting " + file + " == ")
                 proc = Popen(file, shell=True)
                 processes[file] = proc
-
-        keys = list(processes ) # get keys
-
-        for key in keys:
-            if processes[key].poll() is not None:
-                # It's no longer alive
-                processes.pop(key, None)
+            else:
+                if processes[file].poll() is not None:
+                    # It's no longer alive
+                    processes.pop(file, None)
