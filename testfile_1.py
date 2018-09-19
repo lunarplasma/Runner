4d4d142e252df761f7e04fd2b1a24cf9d1df365f
ITCS\Johng <lunarplasma@gmail.com>
2018-09-19 15:15:18 +0100

    Removed testfiles from root.

--- testfile_1.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-
-
-# wait 2 seconds, then raise an exception
-print("testfile_1 has started!")
-sleep(2)
-raise Exception("I'm causing myself to fall over.")
--- testfile_2.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-
-
-# wait 2 seconds, then raise an exception
-print("testfile_2 has started!")
-sleep(5)
-raise Exception ("I also fell over")
